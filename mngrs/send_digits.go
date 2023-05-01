5d842da12efe9338a76bc18e896218a0a84ec125
Nadir Hamid <contact@infinitet3ch.com>
2023-05-01 16:23:36 -0600

    integrate send_digits.go code

--- mngrs/send_digits.go
+++ mngrs/send_digits.go
@@ -2,8 +2,10 @@ package mngrs
 import (
 	//"context"
 	//"github.com/CyCoreSystems/ari/v5"
-
+	"github.com/sirupsen/logrus"
+	"lineblocs.com/processor/utils"
 	"lineblocs.com/processor/types"
+	helpers "github.com/Lineblocs/go-helpers"
 )
 type SendDigitsManager struct {
 	ManagerContext *types.Context
@@ -19,4 +21,22 @@ func NewSendDigitsManager(mngrCtx *types.Context, flow *types.Flow) (*SendDigits
 }
 func (man *SendDigitsManager) StartProcessing() {
 	//log := man.ManagerContext.Log
+
+	cell := man.ManagerContext.Cell
+	//model := cell.Model
+	data := cell.Model.Data
+	next, _ := utils.FindLinkByName(cell.SourceLinks, "source", "Finished")
+	keys := data["text"].(types.ModelDataStr).Value
+	lineChannel := man.ManagerContext.Channel
+	//dtmfOpts := &ari.DTMFOptions{}
+	for i := 0; i < len(keys); i++ {
+    	key:= string( keys[i] )
+		helpers.Log(logrus.DebugLevel, "sending DTMF " + key)
+		//lineChannel.Channel.SendDTMF(key, dtmfOpts)
+		lineChannel.Channel.SendDTMF(key, nil)
+ 	}
+	resp := types.ManagerResponse{
+		Channel: lineChannel,
+		Link:    next}
+	man.ManagerContext.RecvChannel <- &resp
 }
